ca6e8af83ae206d859f1992e51dd393db8d5c4af
nidhia <nidhia@gmail.com>
2024-05-21 01:13:32 +0530

    second trial

---
+++ firstgit.java
@@ -0,0 +1,15 @@
+package git;
+
+public class firstgit {
+int i=10;
+void run() {
+	System.out.println("running");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+firstgit s1= new firstgit();
+s1.run();
+	}
+
+}
