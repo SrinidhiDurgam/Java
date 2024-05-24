97f7ca6a7c5fc83652a8867eee6c64c7328ba5a1
nidhia <nidhia@gmail.com>
2024-05-24 16:45:42 +0530

    new file

---
+++ firstjava/src/firstjava/java.java
@@ -0,0 +1,17 @@
+package firstjava;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String s="srinidhi";
+String t="  ";
+for(int i=s.length()-1;i>=0;i--) {
+	t=t+s.charAt(i);
+}
+System.out.println(t);
+	}
+
+}
+
