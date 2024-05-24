14751cdb278164ce8bb47d578406d00f474229fa
nidhia <nidhia@gmail.com>
2024-05-24 22:49:43 +0530

    change

--- firstjava/src/firstjava/java.java
+++ firstjava/src/firstjava/java.java
@@ -5,7 +5,7 @@ public class java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-String s="srinidhi";
+String s="nidhia";
 String t="  ";
 for(int i=s.length()-1;i>=0;i--) {
 	t=t+s.charAt(i);
